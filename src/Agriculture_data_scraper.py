762c45aa1578b19d3fe7122a279cb39326ea58b8
Mobin al hassan <mobinalhassan1@gmail.com>
2020-05-04 04:29:43 +0500

    f96_401+_158 with w8

--- src/Agriculture_data_scraper.py
+++ src/Agriculture_data_scraper.py
@@ -111,7 +111,7 @@ class FenceInstallerScraper:
 
     def save_excel_file(self):
         dataframe = pd.DataFrame(self.fence_installers)
-        dataframe.to_excel(get_full_path("../data/All_Fence_installers_facebook_31.xlsx"), engine='xlsxwriter')
+        dataframe.to_excel(get_full_path("../data/All_Fence_installers_facebook_96.xlsx"), engine='xlsxwriter')
         # writer = pd.ExcelWriter(get_full_path("../data/All_Fence_installers.xlsx"), engine='xlsxwriter', options={'strings_to_urls': False})
         # dataframe.to_excel(writer)
         print(f'File saved! Records ==> {len(self.fence_installers)}')
